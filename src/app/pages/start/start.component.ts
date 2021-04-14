84cb19364f1827415260890bcef4d31de04055a4
mikezielony <mike.zielony@gmail.com>
2021-04-14 08:15:31 +0200

    Add popup window

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -4,7 +4,6 @@ import {AccountResultModel} from '../../models/accountResult.model';
 import {take} from 'rxjs/operators';
 
 
-
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
@@ -16,18 +15,36 @@ export class StartComponent implements OnInit {
   utc = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
   id: number;
   topic: string;
+  isAuthPopVisible = false;
+  isUserLogin = false;
+  loginPictures: ILoginInterface[] = [{id: 'raz', pic: 'kotek.jpg'}, {id: 'dwa', pic: 'kotek.jpg'}, {id: 'trzy', pic: 'kotek.jpg'}];
 
-  constructor() {}
+  constructor() {
+  }
 
-  public getIndex(kidName: string) {
-    if (kidName === 'Róża'){
+  public getIndex(kidName: string): void {
+    this.isAuthPopVisible = true;
+    if (kidName === 'Róża') {
       this.index = 1;
-    }else if (kidName === 'Sara'){
+    } else if (kidName === 'Sara') {
       this.index = 0;
     }
   }
 
   ngOnInit(): void {
+    // this.loginPictures = [...this.loginPictures]
 
   }
+  public setImage(id): void {
+    if (id === 'raz') {
+      this.isAuthPopVisible = false;
+      this.isUserLogin = true;
+    }
+
+}
+}
+
+export interface ILoginInterface {
+  id: string;
+  pic: string;
 }
